9dfa4b024f3eb3e6ecb82abe0b9c2948cdb8b23f
steamjin <kkjj2024@naver.com>
2026-01-30 16:28:42 +0900

    init

---
+++ src/main/java/com/teamgold/goldenharvestuser/GoldenHarvestUserApplication.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenHarvestUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenHarvestUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamgold/goldenharvestuser/GoldenHarvestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenHarvestUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
